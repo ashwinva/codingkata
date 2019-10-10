f71f65e3a4a38eb37e5a9876d2b4f2047d9097a9
ashwinva <ashwinvarghese2@gmail.com>
2019-10-10 19:58:43 +0100

    Initial Commit

---
+++ src/main/java/main.java
@@ -0,0 +1,11 @@
+
+import org.apache.logging.log4j.Logger;
+public class main {
+
+    private Logger logger;
+
+    public static void main (String args[]){
+
+        System.out.println("Hello World!!!");
+    }
+}
